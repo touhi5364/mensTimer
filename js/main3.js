75e0fa1892807e47c249fb3f5fc6893272f23fb1
古川雅也 <furukawamasaya@furukawBookpuro.lan>
2018-08-17 05:33:40 +0900

    文字列変更

--- js/main3.js
+++ js/main3.js
@@ -7,12 +7,16 @@ $(window).on('load',function(){
 
 	if(taste == 'ton'){
 		source = 'url("images/豚骨.PNG")';
+		$(".texts p").text("豚骨");
 	}else if(taste == 'sio'){
 		source = 'url("images/塩.PNG")';
+		$(".texts p").text("塩");
 	}else if(taste == 'syoyu'){
 		source = 'url("images/醬油.PNG")';
+		$(".texts p").text("しょゆ");
 	}else if(taste == 'miso'){
 		source = 'url("images/味噌.PNG")';
+		$(".texts p").text("味噌");
 	}
 
 	$('body').css('background-image',source);
