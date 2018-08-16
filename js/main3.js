14998c6998b7725b8d9d91186e53d475c20bdc2d
古川雅也 <furukawamasaya@furukawBookpuro.lan>
2018-08-17 06:22:55 +0900

    完成

--- js/main3.js
+++ js/main3.js
@@ -7,16 +7,16 @@ $(window).on('load',function(){
 
 	if(taste == 'ton'){
 		source = 'url("images/豚骨.PNG")';
-		$(".texts p").text("豚骨");
+		$(".texts p").text("ありのままの俺をお届けするぜぇええ！ヒョウッ！！！");
 	}else if(taste == 'sio'){
 		source = 'url("images/塩.PNG")';
-		$(".texts p").text("塩");
+		$(".texts p").text("たまたまラーメン余ったからやるよ…！黙って食えっ！！");
 	}else if(taste == 'syoyu'){
 		source = 'url("images/醬油.PNG")';
-		$(".texts p").text("しょゆ");
+		$(".texts p").text("冷めないように、懐であっためといたZE！");
 	}else if(taste == 'miso'){
 		source = 'url("images/味噌.PNG")';
-		$(".texts p").text("味噌");
+		$(".texts p").text("おじさんの手料理を喰らえ♪");
 	}
 
 	$('body').css('background-image',source);
